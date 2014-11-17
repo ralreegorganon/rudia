256ed2230171f9b87fedb69878f01cf99118d03b
Jason Jones <jasonedwardjones@gmail.com>
2014-11-17 11:22:13 -0900

    Split client into client and upstream

--- rudia.go
+++ rudia.go
@@ -10,56 +10,71 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-type client struct {
-	incoming    chan string
-	outgoing    chan string
-	dead        chan bool
-	isUpstream  bool
-	conn        net.Conn
-	reader      *bufio.Reader
-	writer      *bufio.Writer
-	idleTimeout time.Duration
+type upstream struct {
+	readMessages chan string
+	dead         chan bool
+	conn         net.Conn
+	reader       *bufio.Reader
+	idleTimeout  time.Duration
 }
 
-func newClient(conn net.Conn, isUpstream bool, idleTimeout time.Duration) *client {
+func newUpstream(conn net.Conn, idleTimeout time.Duration) *upstream {
 	r := bufio.NewReader(conn)
-	w := bufio.NewWriter(conn)
-	c := &client{
-		incoming:    make(chan string),
-		outgoing:    make(chan string),
-		dead:        make(chan bool),
-		isUpstream:  isUpstream,
-		conn:        conn,
-		reader:      r,
-		writer:      w,
-		idleTimeout: idleTimeout,
+	u := &upstream{
+		readMessages: make(chan string),
+		dead:         make(chan bool),
+		conn:         conn,
+		reader:       r,
+		idleTimeout:  idleTimeout,
 	}
-	c.listen()
-	return c
+
+	go u.read()
+
+	return u
 }
 
-func (c *client) read() {
+func (u *upstream) read() {
 	for {
-		if c.isUpstream {
-			c.conn.SetReadDeadline(time.Now().Add(c.idleTimeout))
-		}
-		line, err := c.reader.ReadString('\n')
+		u.conn.SetReadDeadline(time.Now().Add(u.idleTimeout))
+		line, err := u.reader.ReadString('\n')
 		if err != nil {
 			log.WithFields(log.Fields{
-				"err":    err,
-				"client": c.conn.RemoteAddr(),
-			}).Warn("Failed to read from client")
+				"err":      err,
+				"upstream": u.conn.RemoteAddr(),
+			}).Warn("Failed to read from upstream")
 
-			c.dead <- true
+			u.dead <- true
 			break
 		}
 
-		c.incoming <- line
+		u.readMessages <- line
 	}
 }
 
+type client struct {
+	messagesToWrite chan string
+	dead            chan bool
+	conn            net.Conn
+	writer          *bufio.Writer
+	idleTimeout     time.Duration
+}
+
+func newClient(conn net.Conn) *client {
+	w := bufio.NewWriter(conn)
+	c := &client{
+		messagesToWrite: make(chan string),
+		dead:            make(chan bool),
+		conn:            conn,
+		writer:          w,
+	}
+
+	go c.write()
+
+	return c
+}
+
 func (c *client) write() {
-	for data := range c.outgoing {
+	for data := range c.messagesToWrite {
 		_, err := c.writer.WriteString(data)
 		if err != nil {
 			log.WithFields(log.Fields{
@@ -80,11 +95,6 @@ func (c *client) write() {
 	}
 }
 
-func (c *client) listen() {
-	go c.read()
-	go c.write()
-}
-
 type RepeaterOptions struct {
 	RetryInterval time.Duration
 	IdleTimeout   time.Duration
@@ -93,13 +103,10 @@ type RepeaterOptions struct {
 // A Repeater connects to an upstream TCP endpoint and relays the messages
 // it receives to all connected clients.
 type Repeater struct {
-	relayClients    map[string]*client
-	upstreamClients map[string]*client
-	relayJoins      chan net.Conn
-	upstreamJoins   chan net.Conn
-	outgoing        chan string
-	incoming        chan string
-	upstreamFault   chan bool
+	clients         map[string]*client
+	upstreams       map[string]*upstream
+	readMessages    chan string
+	messagesToWrite chan string
 	options         *RepeaterOptions
 }
 
@@ -107,13 +114,10 @@ type Repeater struct {
 // connections.
 func NewRepeater(options *RepeaterOptions) *Repeater {
 	r := &Repeater{
-		relayClients:    make(map[string]*client),
-		upstreamClients: make(map[string]*client),
-		relayJoins:      make(chan net.Conn),
-		upstreamJoins:   make(chan net.Conn),
-		incoming:        make(chan string),
-		outgoing:        make(chan string),
-		upstreamFault:   make(chan bool),
+		clients:         make(map[string]*client),
+		upstreams:       make(map[string]*upstream),
+		readMessages:    make(chan string),
+		messagesToWrite: make(chan string),
 		options:         options,
 	}
 	r.listen()
@@ -126,12 +130,13 @@ func NewRepeater(options *RepeaterOptions) *Repeater {
 // until the program exits.
 func (r *Repeater) Proxy(address string) {
 	go func() {
+		fault := make(chan bool)
 		for {
 			log.WithFields(log.Fields{
 				"upstream": address,
 			}).Info("Dialing upstream")
 
-			c, err := net.Dial("tcp", address)
+			conn, err := net.Dial("tcp", address)
 			if err != nil {
 				log.WithFields(log.Fields{
 					"upstream": address,
@@ -146,8 +151,8 @@ func (r *Repeater) Proxy(address string) {
 				time.Sleep(r.options.RetryInterval)
 				continue
 			}
-			r.upstreamJoins <- c
-			_ = <-r.upstreamFault
+			r.joinUpstream(conn, fault)
+			_ = <-fault
 		}
 	}()
 }
@@ -179,42 +184,59 @@ func (r *Repeater) ListenAndAccept(address string) {
 			}).Error("Unable to accept client")
 			continue
 		}
-		r.relayJoins <- conn
+
+		r.joinClient(conn)
 	}
 }
 
 func (r *Repeater) broadcast(data string) {
-	for _, c := range r.upstreamClients {
-		c.outgoing <- data
+	for _, c := range r.clients {
+		c.messagesToWrite <- data
 	}
 }
 
-func (r *Repeater) join(conn net.Conn, clients map[string]*client, isUpstream bool) {
+func (r *Repeater) joinUpstream(conn net.Conn, fault chan bool) {
 	log.WithFields(log.Fields{
-		"client":     conn.RemoteAddr(),
-		"isUpstream": isUpstream,
-	}).Info("Creating client")
+		"upstream": conn.RemoteAddr(),
+	}).Info("Creating upstream")
 
-	c := newClient(conn, isUpstream, r.options.IdleTimeout)
-	clients[conn.RemoteAddr().String()] = c
+	u := newUpstream(conn, r.options.IdleTimeout)
+	r.upstreams[conn.RemoteAddr().String()] = u
 	go func() {
 		for {
 			select {
-			case inc := <-c.incoming:
-				if isUpstream {
-					r.incoming <- inc
-				}
+			case message := <-u.readMessages:
+				r.readMessages <- message
+			case _ = <-u.dead:
+				log.WithFields(log.Fields{
+					"upstream": u.conn.RemoteAddr(),
+				}).Warn("Dead upstream")
+
+				deadRemote := u.conn.RemoteAddr().String()
+				delete(r.upstreams, deadRemote)
+				fault <- true
+			}
+		}
+	}()
+}
+
+func (r *Repeater) joinClient(conn net.Conn) {
+	log.WithFields(log.Fields{
+		"client": conn.RemoteAddr(),
+	}).Info("Creating client")
+
+	c := newClient(conn)
+	r.clients[conn.RemoteAddr().String()] = c
+	go func() {
+		for {
+			select {
 			case _ = <-c.dead:
 				log.WithFields(log.Fields{
-					"client":     c.conn.RemoteAddr(),
-					"isUpstream": isUpstream,
+					"client": c.conn.RemoteAddr(),
 				}).Warn("Dead client")
 
 				deadRemote := c.conn.RemoteAddr().String()
-				delete(clients, deadRemote)
-				if isUpstream {
-					r.upstreamFault <- true
-				}
+				delete(r.clients, deadRemote)
 			}
 		}
 	}()
@@ -224,12 +246,8 @@ func (r *Repeater) listen() {
 	go func() {
 		for {
 			select {
-			case data := <-r.incoming:
+			case data := <-r.readMessages:
 				r.broadcast(data)
-			case conn := <-r.upstreamJoins:
-				r.join(conn, r.upstreamClients, true)
-			case conn := <-r.relayJoins:
-				r.join(conn, r.upstreamClients, false)
 			}
 		}
 	}()
